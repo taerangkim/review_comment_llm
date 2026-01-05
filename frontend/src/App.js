9ee41ab51aede9846583e2e25e1f14eb4d2b6db7
muse7244 <taerangkim0116@gmail.com>
2026-01-05 14:48:43 +0900

    kobert test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './component/main';
+import Main2 from './component/main2';
 
 function App() {
   return (
-    <Main />
+    <Main2 />
   );
 }
 
